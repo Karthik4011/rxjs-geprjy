0fa7da0092f10b78eaada6a8045cd438f4cac4ca
Karthik4011 <dattu.sai4@gmail.com>
2020-06-28 22:31:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-geprjy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-geprjy)
---
+++ index.ts
@@ -0,0 +1,27 @@
+import { Observable, Subscriber } from 'rxjs';
+
+
+const observable = new Observable(
+  subscriber => {
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+
+    setTimeout(()=>{
+      subscriber.next(4);
+      subscriber.complete();
+    },2000)
+  }
+)
+
+console.log("I am about to subscribe");
+
+const subscriber = observable.subscribe({
+  next(x){console.log(`we got: ${x}`)},
+  error(err){console.log("error is : "+err)},
+  complete(){ console.log("completed")}
+})
+
+subscriber.unsubscribe()
+
+console.log("all Done")
